0e0b1d42c760bb6f46849463e60fc1bd0ed01877
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-04-27 21:20:57 -0700

    Add api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,19 +2,26 @@
 '''Start a Flask web app
 '''
 import os
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
 def close_session(exception=None):
-    '''Close storage on teardown
-    '''
+    '''Close storage on teardown'''
+    return jsonify({'error': 'Not found'})
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    '''Return a 404 status code response'''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
