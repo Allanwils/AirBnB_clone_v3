4198a68f80b6971192d9ec76d4a1958c5207c314
Allan Masiga <zionalehebrew@gmail.com>
2023-04-29 07:58:18 +0300

    updated app

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,14 @@ import os
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+# Create CORS instance
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_session(exception=None):
