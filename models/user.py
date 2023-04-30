40d48d814de11824da9513f4e0753834fc1b340d
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2023-04-30 10:40:01 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -14,22 +14,22 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        _password = Column('password', String(128), nullable=False)
+        password = Column('password', String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
 
-        @property
-        def password(self):
-            """ Getter for password """
-            return self._password
+#         @property
+#         def password(self):
+#             """ Getter for password """
+#             return self._password
 
-        @password.setter
-        def password(self, value):
-            """ Setter for password """
-            if value:
-                self._password = md5(value.encode()).hexdigest()
+#         @password.setter
+#         def password(self, value):
+#             """ Setter for password """
+#             if value:
+#                 self._password = md5(value.encode()).hexdigest()
 
     else:
         email = ""
